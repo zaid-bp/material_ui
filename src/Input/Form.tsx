82b689f27705d3b84baba9172f47d05d3abd5c04
zaid-bp <zaid@bitspro.com>
2023-08-29 19:15:36 +0500

    dropdown added

--- src/Input/Form.tsx
+++ src/Input/Form.tsx
@@ -1,12 +1,35 @@
-import { TextField, Typography, Stack, Button } from '@mui/material';
+import { TextField, Typography, Stack, Button, FilledTextFieldProps, OutlinedTextFieldProps, StandardTextFieldProps, TextFieldVariants } from '@mui/material';
 import { Formik, Field, Form, FormikHelpers, ErrorMessage } from 'formik';
 import * as yup from 'yup';
+import Autocomplete from '@mui/material/Autocomplete';
+import { JSX } from 'react/jsx-runtime';
 
+
+const cities = [
+'Karachi',
+'Lahore',
+'Islamabad',
+'Faisalabad',
+'Rawalpindi',
+'Multan',
+'Peshawar',
+'Quetta',
+'Sialkot',
+'Gujranwala'
+];
+const provinces = [
+  "Punjab",
+  "Sindh",
+  "Khyber Pakhtunkhwa",
+  "Balochistan",
+  "Gilgit-Baltistan",
+  "Azad Jammu and Kashmir"
+];
 const validationSchema = yup.object().shape({
   firstName: yup.string().required('First Name is required'),
   lastName: yup.string().required('Last Name is required'),
   city: yup.string().required('City is required'),
-  state: yup.string().required('State is required'),
+  selectedProvince: yup.string().required('State is required'),
   addLine2: yup.string().required('addredd is required'),
   addLine1: yup.string().required('addredd is required'),  
 });
@@ -28,7 +51,7 @@ interface Values {
   firstName: string;
   lastName: string;
   city: string;
-  state:string;
+  selectedProvince:string;
   addLine1:string;
   addLine2:string;
 }
@@ -43,16 +66,19 @@ function Myform() {
           firstName: '',
           lastName: '',
           city: '',
-          state:'',
+          selectedProvince:'',
           addLine1:'',
           addLine2:'',
         }}
 
         validationSchema={validationSchema}
+        
         onSubmit={(
           values: Values,
           { setSubmitting }: FormikHelpers<Values>
+          
         ) => {
+          
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
@@ -61,7 +87,7 @@ function Myform() {
         
       >
 
-{({ errors }) => (
+{({ errors, values, handleChange, handleBlur }) => (
   <Form style={formStyle}>
   <Stack>
   <Typography variant='h4' fontWeight={'bold'}>SignUp</Typography>
@@ -120,7 +146,7 @@ function Myform() {
       />
       
       <Stack direction={'row'} spacing={2}>
-        <Field 
+        {/* <Field 
           id="standard-basic" 
           as={TextField} 
           name='city' 
@@ -128,10 +154,8 @@ function Myform() {
           variant="outlined"
           helperText={<ErrorMessage name="city" />}
           error={Boolean(errors.city)?true:false}
-
-        />
-        
-          <Field 
+        /> */}
+          {/* <Field 
           id="standard-basic" 
           as={TextField} 
           name='state' 
@@ -139,8 +163,47 @@ function Myform() {
           variant="outlined"
           helperText={<ErrorMessage name="state" />}
           error={Boolean(errors.state)?true:false}
+        /> */}
+          <Autocomplete
+            name="selectedProvince"
+            value={values.selectedProvince}
+            onChange={(_event, stateVal) => handleChange('selectedProvince')(stateVal)}
+            fullWidth
+            // onBlur={handleBlur('city')}
+            options={provinces}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Select a State"
+                variant="outlined"
+                error={Boolean(errors.selectedProvince)}
+                helperText={<ErrorMessage name="selectedProvince" />}
+              />
+            )}
+          />
 
-        />
+          <Autocomplete
+            name="city"
+            value={values.city}
+            onChange={(_event, cityVal) => handleChange('city')(cityVal)}
+            fullWidth
+            // onBlur={handleBlur('city')}
+            options={cities}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Select a City"
+                variant="outlined"
+                error={Boolean(errors.city)}
+                helperText={<ErrorMessage name="city" />}
+              />
+            )}
+          />
+
+
+
+
+
       </Stack>
 
     <Stack alignItems={'center'}>
