c1a879d8adbf4b29b0ee20f058d40b7c79481964
zaid-bp <zaid@bitspro.com>
2023-09-07 15:16:18 +0500

    responsive

--- src/Dashboard/NewAdd.tsx
+++ src/Dashboard/NewAdd.tsx
@@ -9,107 +9,37 @@ const add = [
     {
         name: 'Muhammad Haris',
         mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
+        planType: 'block',
+        nextBillingDate: '20-2-2023',
+        icon: 'https://img.freepik.com/free-photo/portrait-handsome-smiling-stylish-young-man-model-dressed-red-checkered-shirt-fashion-man-posing_158538-4909.jpg?w=1060&t=st=1694081329~exp=1694081929~hmac=2ac7293a446246eb441e8a8ec3194c1710acb3b27e2c305f8b092eef4e09f625'
     },
     {
         name: 'Saqib Bhai',
-        mothlyFee: 1800,
+        mothlyFee: 1700,
         planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
+        nextBillingDate: '30-3-2023',
+        icon: 'https://img.freepik.com/free-photo/handsome-man-outdoors-drinking-coffee-with-sunglasses-guy-with-beard-instagram-effect_1212-818.jpg?w=1380&t=st=1694081361~exp=1694081961~hmac=9906b94e6f04c7b558a7786ce1e17829bb6bfe9cd27e3714ded019f6d14aa548'
     },
     {
         name: 'Muhammad Jumman',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
+        mothlyFee: 1600,
+        planType: 'premium',
+        nextBillingDate: '4-2-2023',
+        icon: 'https://img.freepik.com/premium-photo/onfident-handsome-man-with-arms-crossed-body-smiling-looking-determined_911620-3113.jpg?w=996'
     },
     {
         name: 'Muhammad Ali',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
+        mothlyFee: 18500,
+        planType: 'new',
+        nextBillingDate: '6-2-2023',
+        icon: 'https://img.freepik.com/free-photo/handsome-man-white-shirt-posing-attractive-guy-with-fashion-hairstyle-confident-man-with-short-beard-adult-boy-with-brown-hair-closeup-portrait_186202-8538.jpg?w=1060&t=st=1694081472~exp=1694082072~hmac=1fcb50a742c3cb0ffed11f97c11bd26790bb6f10e034d5063645f4e887468fa3'
     },
     {
         name: 'Mustafa Ahmed',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
-    },
-    {
-        name: 'Muhammad Haris150',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
-    },
-    {
-        name: 'Muhammad Haris150',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
-    },
-    {
-        name: 'Muhammad Haris150',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
-    },
-    {
-        name: 'Muhammad Haris150',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
-    },
-    {
-        name: 'Muhammad Haris150',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
-    },
-    {
-        name: 'Muhammad Haris150',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
-    },
-    {
-        name: 'Muhammad Haris150',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
-    },
-    {
-        name: 'Muhammad Haris150',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
-    },
-    {
-        name: 'Muhammad Haris150',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
-    },
-    {
-        name: 'Muhammad Haris150',
-        mothlyFee: 1800,
-        planType: 'flex',
-        nextBillingDate: '10-20-2023',
-        icon: ''
+        mothlyFee: 2800,
+        planType: 'premium',
+        nextBillingDate: '8-2-2023',
+        icon: 'https://img.freepik.com/free-photo/portrait-serious-concentrated-man-white-shirt_171337-17406.jpg?t=st=1694081464~exp=1694082064~hmac=e2b76e78141859d4d3438f053443a955d6b3d529f21cceb38c5c44b8253000ce'
     },
 ]
 const NewAdd = () => {
--- src/Dashboard/Slider.tsx
+++ src/Dashboard/Slider.tsx
@@ -1,7 +1,6 @@
 import { Avatar, Button, Divider, Paper, Stack, Typography } from "@mui/material";
 import { useEffect, useRef } from "react";
 import { slideInView, normalSlide, txtStyle, sliderFont } from "./Styles";
-import image from "../assets/manImage.png";
 
 const Slider: React.FC<{ data: any, index: number, val: number }> = ({ data, index, val }) => {
     const cardRef = useRef<HTMLDivElement | null>(null)
@@ -9,14 +8,14 @@ const Slider: React.FC<{ data: any, index: number, val: number }> = ({ data, ind
         if (cardRef.current)
             cardRef.current.scrollIntoView({ behavior: 'smooth', inline: 'center' })
     }
-    const sliderActionBtn={
-        fontWeight: 'semi-bold', 
+    const sliderActionBtn = {
+        fontWeight: 'semi-bold',
         padding: '0.2rem 2.5rem',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        margin:'0 !important',
-        paddingBottom:'0 !important'
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '0 !important',
+        paddingBottom: '0 !important'
 
     }
 
@@ -24,21 +23,21 @@ const Slider: React.FC<{ data: any, index: number, val: number }> = ({ data, ind
         slideIntoView()
     }, [val])
 
-      // Add a window resize event listener to re-scroll when the window is resized
-      useEffect(() => {
+    // Add a window resize event listener to re-scroll when the window is resized
+    useEffect(() => {
         window.addEventListener('resize', slideIntoView);
-    
+
         // Cleanup: Remove the event listener when the component unmounts
         return () => {
-          window.removeEventListener('resize', slideIntoView);
+            window.removeEventListener('resize', slideIntoView);
         };
-      }, []);
-    const { name, mothlyFee, planType, nextBillingDate } = data;
+    }, []);
+    const { name, mothlyFee, planType, nextBillingDate, icon } = data;
     return (
         <Paper
             ref={index === val ? cardRef : null}
             component={Stack}
-            sx={{ padding: '1rem 0', borderRadius:'12px',opacity:val === index ?'':'0.7'}}
+            sx={{ padding: '1rem 0', borderRadius: '12px', opacity: val === index ? '' : '0.7' }}
             elevation={4}
             style={val === index ? slideInView : normalSlide}
         >
@@ -52,7 +51,7 @@ const Slider: React.FC<{ data: any, index: number, val: number }> = ({ data, ind
 
                 <Stack direction={'row'} sx={{ position: 'relative', padding: '1.5rem 0' }}>
                     <Stack>
-                        <Avatar src={image} sx={{ width: '7rem', height: '7rem', borderRadius: '3.5rem', marginBottom: '10px' }} />
+                        <Avatar src={icon} sx={{ width: '7rem', height: '7rem', borderRadius: '3.5rem', marginBottom: '10px' }} />
                         <Typography variant="h2" style={txtStyle}>{name}</Typography>
                     </Stack>
                     <Stack marginLeft={'auto'} textAlign={'right'} justifyContent={'space-between'} paddingBottom={6} paddingTop={2}>
@@ -60,16 +59,19 @@ const Slider: React.FC<{ data: any, index: number, val: number }> = ({ data, ind
                         <Typography variant="subtitle1">Skip</Typography>
                     </Stack>
                 </Stack>
-                <Stack direction={'row'} paddingBottom={2}>
-                    <Stack direction={'column'} spacing={1}>
-                        <Typography color={'#463C3C'} style={sliderFont} variant="subtitle2" >Monthly Fee</Typography>
-                        <Typography color={'#463C3C'} style={sliderFont} variant="subtitle2" >Current Plan</Typography>
-                        <Typography color={'#463C3C'} style={sliderFont} variant="subtitle2" >Next Billing Date</Typography>
-                    </Stack>
-                    <Stack direction={'column'} spacing={1} paddingLeft={2}>
-                        <Typography color={'#7B7B7B'} style={sliderFont} variant="subtitle2" >{mothlyFee}</Typography>
-                        <Typography color={'#7B7B7B'} style={sliderFont} variant="subtitle2" >{planType}</Typography>
-                        <Typography color={'#7B7B7B'} style={sliderFont} variant="subtitle2" >{nextBillingDate}</Typography>
+                <Stack direction={'row'} paddingBottom={2} flexWrap={'wrap'}>
+                    <Stack direction={'row'}>
+
+                        <Stack direction={'column'} spacing={1}>
+                            <Typography color={'#463C3C'} style={sliderFont} variant="subtitle2" >Monthly Fee</Typography>
+                            <Typography color={'#463C3C'} style={sliderFont} variant="subtitle2" >Current Plan</Typography>
+                            <Typography color={'#463C3C'} style={sliderFont} variant="subtitle2" >Next Billing Date</Typography>
+                        </Stack>
+                        <Stack direction={'column'} spacing={1} paddingLeft={2}>
+                            <Typography color={'#7B7B7B'} style={sliderFont} variant="subtitle2" >{mothlyFee}</Typography>
+                            <Typography color={'#7B7B7B'} style={sliderFont} variant="subtitle2" >{planType}</Typography>
+                            <Typography color={'#7B7B7B'} style={sliderFont} variant="subtitle2" >{nextBillingDate}</Typography>
+                        </Stack>
                     </Stack>
                     <Stack direction={'row'} spacing={2} marginLeft={'auto'} alignSelf={'flex-end'}>
                         <Button variant="contained" className="approve" style={sliderActionBtn}>Approve</Button>
